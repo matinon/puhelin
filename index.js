f3be0c923c64fd134ac57078f16c3207f3f7bc00
matinon <matiasmarsynaho@kamk.fi>
2024-06-12 17:18:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Käytä Morgania loggausta varten
+app.use(morgan('tiny'));
+
+// Middleware JSON-datan käsittelemiseksi
+app.use(express.json());
+
+let persons = [
+  { 
+    id: 1, 
+    name: 'Arto Hellas', 
+    number: '040-123456' 
+  },
+  { 
+    id: 2, 
+    name: 'Ada Lovelace', 
+    number: '39-44-5323523' 
+  },
+  { 
+    id: 3, 
+    name: 'Dan Abramov', 
+    number: '12-43-234345' 
+  },
+  { 
+    id: 4, 
+    name: 'Mary Poppendieck', 
+    number: '39-23-6423122' 
+  }
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const numberOfPersons = persons.length;
+  const currentTime = new Date();
+  
+  const responseText = `
+    <p>Phonebook has info for ${numberOfPersons} people</p>
+    <p>${currentTime}</p>
+  `;
+  
+  res.send(responseText);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send({ error: 'Person not found' });
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const initialLength = persons.length;
+  persons = persons.filter(p => p.id !== id);
+
+  if (persons.length < initialLength) {
+    res.status(204).end();
+  } else {
+    res.status(404).send({ error: 'Person not found' });
+  }
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name or number missing' 
+    });
+  }
+
+  const nameExists = persons.some(person => person.name === body.name);
+  if (nameExists) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
